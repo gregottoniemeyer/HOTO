04fe1bc4e2366fae3086deebf3dc606def3c066d
Gentemann <clwhit13@ndc.nasa.gov>
2024-09-26 10:13:52 -0700

    generalizing and creating config file

---
+++ myconfig.py
@@ -0,0 +1,10 @@
+# myconfig.py:
+
+# Target latitude and longitude Milwaukee Trench
+target_name = 'MilwaukeeTrench'
+target_lat = 19.71361111111111  # Example: Latitude of Milwaukee Trench
+target_lon = -67.31083333333333  # Example: Longitude of Milwaukee Trench
+output_path = './../data/MilwaukeeTrench/'
+
+start_year, start_month, start_day = 2023,12,6
+number_days_to_process = 9
