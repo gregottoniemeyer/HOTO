9a90ce22f8b260027b0973dd7b8799dfb396e26c
Gentemann <clwhit13@ndc.nasa.gov>
2024-10-16 09:20:26 -0700

    run more GOES filenames

--- myconfig.py
+++ myconfig.py
@@ -4,10 +4,15 @@
 target_name = 'MilwaukeeTrench'
 target_lat = 19.71361111111111  # Example: Latitude of Milwaukee Trench
 target_lon = -67.31083333333333  # Example: Longitude of Milwaukee Trench
-output_path = './../data/MilwaukeeTrench/'
+output_path_fname = './data/MilwaukeeTrench_fname/'
+output_path_data = './data/MilwaukeeTrench_data/'
 output_path2 = './../dolphin/'
 
-start_year, start_month, start_day = 2023,12,6
+#start_year, start_month, start_day = 2023,12,6
+#number_days_to_process = 9
+#start_year, start_month, start_day = 2022,12,6
+#number_days_to_process = 9
+start_year, start_month, start_day = 2020,12,6
 number_days_to_process = 9
 
 # Hotel location for rain data
