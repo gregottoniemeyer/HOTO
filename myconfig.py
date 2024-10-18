d4f89d339f8b9af4fc0cb7d614658d2baca0f5ff
Gentemann <clwhit13@ndc.nasa.gov>
2024-10-17 17:40:05 -0700

    nrt data

--- myconfig.py
+++ myconfig.py
@@ -4,16 +4,24 @@
 target_name = 'MilwaukeeTrench'
 target_lat = 19.71361111111111  # Example: Latitude of Milwaukee Trench
 target_lon = -67.31083333333333  # Example: Longitude of Milwaukee Trench
-output_path_fname = './data/MilwaukeeTrench_fname/'
-output_path_data = './data/MilwaukeeTrench_data/'
-output_path2 = './../dolphin/'
+output_path_goes_fname = './data/MilwaukeeTrench_fname/'
+output_path_goes_data = './data/MilwaukeeTrench_data/'
 
-#start_year, start_month, start_day = 2023,12,6
-#number_days_to_process = 9
+output_path_nrt = './data/nrt/'
+
+output_path_buoy_data = './data/buoy/'
+output_path_rain_data = './data/rain/'
+
+output_path_dolphin = './../dolphin/'
+
+start_year, start_month, start_day = 2023,12,6
+number_days_to_process = 9
 #start_year, start_month, start_day = 2022,12,6
 #number_days_to_process = 9
-start_year, start_month, start_day = 2020,12,6
-number_days_to_process = 9
+#start_year, start_month, start_day = 2021,12,6
+#number_days_to_process = 9
+#start_year, start_month, start_day = 2020,12,6
+#number_days_to_process = 9
 
 # Hotel location for rain data
 site_lon = -80.1341  # longitude in degrees
