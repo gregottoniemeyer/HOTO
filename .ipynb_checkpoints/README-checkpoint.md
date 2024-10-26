dac3a83b6ef29c338078c6c15a9fd0fd152e5d14
cgentemann <cgentemann@gmail.com>
2024-10-26 00:04:26 +0000

    nrt_code stuff
    moved stuff around. realtime code stuff all in nrt_code now

---
+++ .ipynb_checkpoints/README-checkpoint.md
@@ -0,0 +1,12 @@
+# ocean-heart
+notebooks for some ocean data, buoys, ecco model, some live streams, some not.
+
+All code here can be run within the 'heart' environment.
+You can create this environment in miniconda:
+
+conda env create -f .\environment.yml
+
+conda activate heart
+
+You can also run online by clicking here:
+[![Binder](https://binder.opensci.2i2c.cloud/badge_logo.svg)](https://binder.opensci.2i2c.cloud/v2/gh/cgentemann/ocean-heart/HEAD)
---
+++ nrt_code/myconfig.py
@@ -0,0 +1,35 @@
+# myconfig.py:
+
+# Target latitude and longitude Milwaukee Trench
+target_name = 'MilwaukeeTrench'
+target_lat = 19.71361111111111  # Example: Latitude of Milwaukee Trench
+target_lon = -67.31083333333333  # Example: Longitude of Milwaukee Trench
+
+output_path_figures = './../figures/'
+output_path_goes_fname = './../data/goes/fname/'
+output_path_goes_data = './../data/goes/data/'
+output_path_buoy_data = './../data/buoy/'
+output_path_rain_data = './../data/rain/'
+output_path_dolphin = './../data/dolphins/'
+output_path_ecco = './../data/ecco/'
+output_path_seismic = './../data/ecco/'
+output_path_nrt = './../data/nrt/'
+
+start_year, start_month, start_day = 2023,12,6
+number_days_to_process = 8
+#start_year, start_month, start_day = 2022,12,6
+#number_days_to_process = 9
+#start_year, start_month, start_day = 2021,12,6
+#number_days_to_process = 9
+#start_year, start_month, start_day = 2020,12,6
+#number_days_to_process = 9
+
+# Hotel location for rain data
+site_lon = -80.1341  # longitude in degrees
+site_lat = 25.7826  # latitdue in degrees
+radar_id = 'KAMX'
+
+#buoy information
+station = '41043'
+buoy_lat, buoy_lon =  19.71361111111111, -67.31083333333333
+
